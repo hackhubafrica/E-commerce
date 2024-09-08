7c4d5fc5bae96292a9a232de4254ccc70f5f5d8f
hackhubafrica <93518934+hackhubafrica@users.noreply.github.com>
2024-09-08 16:49:17 +0300

    Initial commit

---
+++ src/Pages/LoginSignup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const LoginSignup =() => {
+return (
+
+	<div>
+		
+	</div>	
+)
+}
+
+export default LoginSignup
---
+++ src/Pages/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Product =() => {
+return (
+
+	<div>
+		
+	</div>	
+)
+}
+
+export default Product
